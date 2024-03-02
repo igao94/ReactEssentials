eaca00820c314fd220b9bd296972a2f795de647f
igao94 <igao998@gmail.com>
2024-03-02 12:18:03 +0100

    added file structuring

---
+++ src/components/Examples.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+import TabButton from "./TabButton";
+import Tabs from "./Tabs.jsx";
+import Section from "./Section.jsx";
+import { EXAMPLES } from "../data.js";
+import Header from "./Header/Header.jsx";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function selectHandler(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please select topic: </p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        ButtonsContainer="menu"
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => selectHandler("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => selectHandler("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => selectHandler("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => selectHandler("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
+    </Section>
+  );
+}
